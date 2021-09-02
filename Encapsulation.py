15350f7e5f0d8008e272071a04f993e302e4f6f6
sourav4949 <46929852+sourav4949@users.noreply.github.com>
2021-09-02 16:53:54 +0530

    modified and added some details

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,27 +1,35 @@
 class Student:
     def __init__(self):
-        self.__name="Sourav"
-        self.__id=123
+        self.__name = "Sourav"
+        self.__id = 123
+
     def display(self):
         print(self.__name)
         print(self.__id)
 
-s=Student()
+
+s = Student()
 s.display()
 print(s._Student__name)
 """name mangling--In python the private members are not completely hidden. it is stored as the format specified above 
 and is termed as name mangling.Below are the dynamic variable entry into the fields"""
 
+
 class Students:
-    def setNames(self,names):
-        self.names=names
+    def setNames(self, names):
+        self.names = names
+
     def getNames(self):
-        return(self.names)
-    def setIds(self,ids):
-        self.ids=ids
+        return (self.names)
+
+    def setIds(self, ids):
+        self.ids = ids
+
     def getIds(self):
-        return(self.ids)
-s1=Students()
+        return self.ids
+
+
+s1 = Students()
 s1.setIds(1)
 print(s1.getIds())
 s1.setNames("Sourav")
