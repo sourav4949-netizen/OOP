4e4fe2cbf702f10d3c9afa052048da2f5b4dda47
sourav4949 <46929852+sourav4949@users.noreply.github.com>
2021-09-02 13:58:21 +0530

    modified and added some details

--- Course.py
+++ Course.py
@@ -1,17 +1,23 @@
-class  Course:
-    def __init__(self,name,rating):
-        self.name=name
-        self.rating=rating
+"""In this module of OOP we have created a class named Course having member variables as name and list of rating provided by
+student taking the class. and also defined a function avarage which will creat the avg of all of the ratings for a given course
+the variable are automatically created by constructor present defined which will take a string and list as input.
+"""
+
+class Course:
+    def __init__(self, name, rating):
+        self.name = name
+        self.rating = rating
 
     def average(self):
-        average=sum(self.rating)/len(self.rating)
-        print("avg rating of",self.name,"is",average)
+        average = sum(self.rating) / len(self.rating)
+        print("avg rating of", self.name, "is", average)
 
-c1=Course('java',[1,2,3,4,5])
+
+c1 = Course('java', [1, 2, 3, 4, 5])
 print(c1.name)
 print(c1.rating)
 c1.average()
-c2=Course('python fundamentals',[5,5,5,5,4])
+c2 = Course('python fundamentals', [5, 5, 5, 5, 4])
 print(c2.name)
 print(c2.rating)
 c2.average()
--- InnerClass.py
+++ InnerClass.py
@@ -1,15 +1,19 @@
 class Car:
-    def __init__(self,make,year):
-        self.make=make
-        self.year=year
+    def __init__(self, make, year):
+        self.make = make
+        self.year = year
+
     class Engine:
-        def __init__(self,number):
-            self.number=number
+        def __init__(self, number):
+            self.number = number
+
         def start(self):
-            print("Engine started",self.number)
+            print("Engine started", self.number)
+
         def __del__(self):
             print("inside the destructor")
 
-c=Car("BMW",2017)
-e=c.Engine(29888)
+
+c = Car("BMW", 2017)
+e = c.Engine(29888)
 e.start()
